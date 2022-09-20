08438dc520488c02a0877d0dc531b4893f0f6229
kahamDeb <98123521+kahamDeb@users.noreply.github.com>
2022-09-20 14:31:52 -0700

    Update wordlist_generator.py

--- wordlist_generator.py
+++ wordlist_generator.py
@@ -6,6 +6,7 @@ value = ascii_letters+digits+punctuation
 for i in range(1,64):
     for j in product(value,repeat=i):
         word = "".join(j)
+        print(word)
         p = open("password.txt","a")
         p.write(word)
         p.write("\n")
